6350e7ac41667737a563f3511f9def7b5f380832
Andrew Schultz <as173171@gmail.com>
2016-01-12 17:50:50 -0500

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 	def index
-		if !current_user
-			redirect_to '/log-in'
-		else
+		if current_user
 			@posts = Post.all
 			@post = Post.new
 			@user = current_user
+		else
+			redirect_to '/log-in'
 		end
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @post = @user.posts
   end
 
   def destroy
