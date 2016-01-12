a909e973e2ab0a0bf4767f4726ffc542114acec5
Niall <niallpatrickmurphy@gmail.com>
2016-01-12 12:24:50 -0500

    building scaffolding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #   helper_method :current_user
-  # # will be available everywhere
+  helper_method :current_user
+  # will be available everywhere
   
-  # def current_user
-  # 	# checks to see if session exists, if so, loads user
-  # 	# and saves into @user instance variable
-  # 	@user ||= User.find(session[:user]) if session [:user_id]
-  # end
+  def current_user
+  	# checks to see if session exists, if so, loads user
+  	# and saves into @user instance variable
+  	@user ||= User.find(session[:user]) if session [:user_id]
+  end
 end
