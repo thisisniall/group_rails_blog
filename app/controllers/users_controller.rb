3bbcd3d35662c7a52623c1c41093de847b3f56b6
Andrew Schultz <as173171@gmail.com>
2016-01-13 17:15:26 -0500

    style style style, also added a time function to posts on home page and a profile card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   		redirect_to '/'
   	else
